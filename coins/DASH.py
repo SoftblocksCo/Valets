8ba91b181047fd74dcae359258c1bd8b0bff7347
pavlovdog <pavlovdog.moscow@gmail.com>
2017-07-31 12:23:54 +0300

    Dash support added

--- coins/DASH.py
+++ coins/DASH.py
@@ -1,3 +1,21 @@
-class DashWallet():
+from subprocess import check_output
+
+class DASH_wallet():
     def __init__(self):
-        pass
+        # Dashd should already be started
+        # Otherwise exception will be raised
+        tmp_address = check_output(['dash-cli', 'getnewaddress'])
+
+    def get_private_key(self, address):
+        """Get private key for address with dash-cli"""
+        private_key = check_output(['dash-cli', 'dumpprivkey', address])
+        private_key = private_key.rstrip() # Remove end line symbol
+
+        return private_key.decode('utf-8')
+
+    def get_address(self):
+        """Generate new address with dash-cli"""
+        address = check_output(['dash-cli', 'getnewaddress'])
+        address = address.rstrip() # Remove end line symbol
+
+        return address.decode('utf-8')
