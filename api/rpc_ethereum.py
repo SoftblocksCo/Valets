e171409278a5ae8fab3284d407d4f3e002a6db8b
pavlovdog <pavlovdog.moscow@gmail.com>
2017-08-13 15:17:38 +0300

    Add sleep parameter

--- api/rpc_ethereum.py
+++ api/rpc_ethereum.py
@@ -2,6 +2,7 @@ from web3 import Web3
 from web3 import KeepAliveRPCProvider
 from glob import glob
 from os.path import expanduser
+from time import sleep
 
 class Ethereum_like_wallet():
     def __init__(self, name, parser):
@@ -16,10 +17,12 @@ class Ethereum_like_wallet():
         # Don't forget to launch geth of geth-classic
         self.web3 = Web3(KeepAliveRPCProvider(host=self.HOST, port=self.PORT)) # connect to RPC
 
-    def get_address(self, passphase):
+    def get_address(self, passphase, sleep_sec=0):
+        sleep(sleep_sec)
         return self.web3.personal.newAccount(passphase)
 
-    def get_keystore_file(self, address):
+    def get_keystore_file(self, address, sleep_sec=0):
+        sleep(sleep_sec)
         cropped_address = address.split('0x')[1]
         keystore_file_path = glob('/'.join([self.ethereum_path, 'keystore/*{}'.format(cropped_address)]))[0]
 
